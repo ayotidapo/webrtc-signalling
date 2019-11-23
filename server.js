8764e11e00f2bd3f5364d103841a73c0315fcf0d
otidapson <oladapo.o@softcom.ng>
2019-11-23 16:47:30 +0100

    working local server

--- server.js
+++ server.js
@@ -7,9 +7,25 @@ app.options("*", cors());
 app.get("/", function(req, res) {
   res.send("<h2>Broadcast server</h2>");
 });
-let port = process.env.PORT || 8080;
-let server = http.listen(port, function() {
-  console.log("listening on *:" + port);
+
+let server = http.listen(3000, function() {
+  console.log("listening on *:3000");
 });
 
 require("./broadcast.js")(server);
+
+// var app = require("express")();
+// var http = require("http").createServer(app);
+// const cors = require("cors");
+// app.use(cors());
+// app.options("*", cors());
+
+// app.get("/", function(req, res) {
+//   res.send("<h2>Broadcast server</h2>");
+// });
+// let port = process.env.PORT || 8080;
+// let server = http.listen(port, function() {
+//   console.log("listening on *:" + port);
+// });
+
+// require("./broadcast.js")(server);
